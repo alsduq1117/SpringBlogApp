bbfac302e2bf44a8c397956c9425353392f193b4
alsduq1117 <alsduq1117@naver.com>
2024-12-11 23:34:40 +0900

    chore : webconfig 주석처리

--- src/main/java/com/blog/api/config/WebConfig.java
+++ src/main/java/com/blog/api/config/WebConfig.java
@@ -1,9 +1,5 @@
 package com.blog.api.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 //@Configuration
 //public class WebConfig implements WebMvcConfigurer {
 //
