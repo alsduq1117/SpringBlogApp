d0a83c294bbfae01295ea5551915a729851c0c21
alsduq1117 <alsduq1117@naver.com>
2025-02-03 18:14:03 +0900

    feat: 로그인 기능 및 비밀번호 암호화

--- src/main/java/com/blog/api/crypto/PasswordEncoder.java
+++ src/main/java/com/blog/api/crypto/PasswordEncoder.java
@@ -1,18 +1,24 @@
 package com.blog.api.crypto;
 
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PasswordEncoder {
 
-    private static final SCryptPasswordEncoder encoder = new SCryptPasswordEncoder(16, 8, 1, 32, 64);
+    private static final SCryptPasswordEncoder encoder = new SCryptPasswordEncoder(
+            16,
+            8,
+            1,
+            32,
+            64);
 
-
-    public String encrypt(String rawPassword) {
+    public String encrpyt(String rawPassword) {
         return encoder.encode(rawPassword);
     }
 
-    public boolean matches(String rawPassword, String encryptedPassword) {
-        return encoder.matches(rawPassword, encryptedPassword);
-
+    public boolean matches(String rawPassword, String encrpytedPassword) {
+        return encoder.matches(rawPassword, encrpytedPassword);
     }
 }
+
