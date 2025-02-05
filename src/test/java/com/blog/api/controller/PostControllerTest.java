5580d9d0f0649ac23a7fdf4b3cacf1bf379b8daa
alsduq1117 <alsduq1117@naver.com>
2025-02-05 22:53:53 +0900

    test: 시큐리티 테스트 코드 작성

--- src/test/java/com/blog/api/controller/PostControllerTest.java
+++ src/test/java/com/blog/api/controller/PostControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -44,66 +45,42 @@ class PostControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "alsduq1117@naver.com", roles = {"ADMIN"})
     @DisplayName("글 작성 요청시 status 반환")
     void test() throws Exception {
         // given
-        PostCreate request = PostCreate.builder()
-                .title("제목입니다.")
-                .content("내용입니다.")
-                .build();
+        PostCreate request = PostCreate.builder().title("제목입니다.").content("내용입니다.").build();
 
         String json = objectMapper.writeValueAsString(request);
 
         // when,then
-        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json)
-                )
-                .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts").contentType(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isOk()).andDo(MockMvcResultHandlers.print());
     }
 
     @Test
+    @WithMockUser(username = "alsduq1117@naver.com", roles = {"ADMIN"})
     @DisplayName("글 작성 요청시 title 값은 필수다.")
     void test2() throws Exception {
         // given
-        PostCreate request = PostCreate.builder()
-                .title("")
-                .content("내용입니다.")
-                .build();
+        PostCreate request = PostCreate.builder().title("").content("내용입니다.").build();
 
         String json = objectMapper.writeValueAsString(request);
 
         // when,then
-        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json)
-                )
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.code").value("400"))
-                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
-                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts").contentType(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isBadRequest()).andExpect(jsonPath("$.code").value("400")).andExpect(jsonPath("$.message").value("잘못된 요청입니다.")).andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요.")).andDo(MockMvcResultHandlers.print());
     }
 
     @Test
-    @DisplayName("글 작성 요청시 DB에 값이 저장된다")
+    @WithMockUser(username = "alsduq1117@naver.com", roles = {"ADMIN"})
+    @DisplayName("글 작성")
     void test3() throws Exception {
         // given
-        PostCreate request = PostCreate.builder()
-                .title("제목입니다.")
-                .content("내용입니다.")
-                .build();
+        PostCreate request = PostCreate.builder().title("제목입니다.").content("내용입니다.").build();
 
         String json = objectMapper.writeValueAsString(request);
 
         // when
-        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json)
-                )
-                .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts").contentType(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isOk()).andDo(MockMvcResultHandlers.print());
 
         //then
         Assertions.assertEquals(1L, postRepository.count());
@@ -115,143 +92,95 @@ class PostControllerTest {
 
 
     @Test
+    @WithMockUser(username = "alsduq1117@naver.com", roles = {"ADMIN"})
     @DisplayName("글 1개 조회")
     void test4() throws Exception {
         // given
-        Post post = Post.builder()
-                .title("123456789012312312")
-                .content("bar")
-                .build();
+        Post post = Post.builder().title("123456789012312312").content("bar").build();
 
         postRepository.save(post);
 
         // expected
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts/{postId}", post.getId())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(post.getId()))
-                .andExpect(jsonPath("$.title").value("1234567890"))
-                .andExpect(jsonPath("$.content").value("bar"))
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts/{postId}", post.getId()).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.id").value(post.getId())).andExpect(jsonPath("$.title").value("1234567890")).andExpect(jsonPath("$.content").value("bar")).andDo(MockMvcResultHandlers.print());
 
     }
 
     @Test
+    @WithMockUser(username = "alsduq1117@naver.com", roles = {"ADMIN"})
     @DisplayName("글 여러개 조회")
     void test5() throws Exception {
         // given
-        List<Post> requestPosts = IntStream.range(0, 20)
-                .mapToObj(i -> Post.builder()
-                        .title("김민엽 제목 " + i)
-                        .content("김민엽 내용 " + i)
-                        .build()
-                )
-                .collect(Collectors.toList());
+        List<Post> requestPosts = IntStream.range(0, 20).mapToObj(i -> Post.builder().title("김민엽 제목 " + i).content("김민엽 내용 " + i).build()).collect(Collectors.toList());
 
         postRepository.saveAll(requestPosts);
 
         // expected
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts?page=1&size=10")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()", Matchers.is(10)))
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts?page=1&size=10").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.length()", Matchers.is(10))).andDo(MockMvcResultHandlers.print());
 
 
     }
 
     @Test
+    @WithMockUser(username = "alsduq1117@naver.com", roles = {"ADMIN"})
     @DisplayName("페이지를 0으로 요청하면 첫 페이지를 가져온다.")
     void test6() throws Exception {
         // given
-        List<Post> requestPosts = IntStream.range(0, 20)
-                .mapToObj(i -> Post.builder()
-                        .title("김민엽 제목 " + i)
-                        .content("김민엽 내용 " + i)
-                        .build()
-                )
-                .collect(Collectors.toList());
+        List<Post> requestPosts = IntStream.range(0, 20).mapToObj(i -> Post.builder().title("김민엽 제목 " + i).content("김민엽 내용 " + i).build()).collect(Collectors.toList());
 
         postRepository.saveAll(requestPosts);
 
         // expected
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts?page=0&size=10")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()", Matchers.is(10)))
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts?page=0&size=10").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.length()", Matchers.is(10))).andDo(MockMvcResultHandlers.print());
 
 
     }
 
     @Test
+    @WithMockUser(username = "alsduq1117@naver.com", roles = {"ADMIN"})
     @DisplayName("글 제목 수정")
     void test7() throws Exception {
         // given
-        Post post = Post.builder()
-                .title("foo")
-                .content("bar")
-                .build();
+        Post post = Post.builder().title("foo").content("bar").build();
 
         postRepository.save(post);
 
-        PostEdit postEdit = PostEdit.builder()
-                .title("foo_edit")
-                .content("bar")
-                .build();
+        PostEdit postEdit = PostEdit.builder().title("foo_edit").content("bar").build();
 
         // expected
-        mockMvc.perform(MockMvcRequestBuilders.patch("/posts/{postId}", post.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(postEdit)))
-                .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.patch("/posts/{postId}", post.getId()).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(postEdit))).andExpect(status().isOk()).andDo(MockMvcResultHandlers.print());
 
 
     }
 
     @Test
+    @WithMockUser(username = "alsduq1117@naver.com", roles = {"ADMIN"})
     @DisplayName("게시글 삭제")
     void test8() throws Exception {
         // given
-        Post post = Post.builder()
-                .title("foo")
-                .content("bar")
-                .build();
+        Post post = Post.builder().title("foo").content("bar").build();
 
         postRepository.save(post);
 
         // expected
-        mockMvc.perform(MockMvcRequestBuilders.delete("/posts/{postId}", post.getId())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/posts/{postId}", post.getId()).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(MockMvcResultHandlers.print());
 
     }
 
     @Test
+    @WithMockUser(username = "alsduq1117@naver.com", roles = {"ADMIN"})
     @DisplayName("존재하지 않는 게시글 조회")
     void test9() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/posts/{postId}", 1L)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound())
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/posts/{postId}", 1L).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound()).andDo(MockMvcResultHandlers.print());
     }
 
     @Test
+    @WithMockUser(username = "alsduq1117@naver.com", roles = {"ADMIN"})
     @DisplayName("존재하지 않는 게시글 수정")
     void test10() throws Exception {
         // given
-        PostEdit postEdit = PostEdit.builder()
-                .title("foo_edit")
-                .content("bar")
-                .build();
+        PostEdit postEdit = PostEdit.builder().title("foo_edit").content("bar").build();
 
         //expected
-        mockMvc.perform(MockMvcRequestBuilders.patch("/posts/{postId}", 1L)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(postEdit)))
-                .andExpect(status().isNotFound())
-                .andDo(MockMvcResultHandlers.print());
+        mockMvc.perform(MockMvcRequestBuilders.patch("/posts/{postId}", 1L).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(postEdit))).andExpect(status().isNotFound()).andDo(MockMvcResultHandlers.print());
     }
 }
