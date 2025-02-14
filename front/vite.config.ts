805a414f59a19e653fac6eb5c9d29aa55cf8fb1f
alsduq1117 <alsduq1117@naver.com>
2025-02-14 13:20:06 +0900

    feat : front 구현

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -9,16 +9,16 @@ export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
-    port : 3000,
+    port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        rewrite: (path) => path.replace(/^\/api/, ""),
+      '/api': {
+        target: 'http://localhost:8080',
+        // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-});
+})
